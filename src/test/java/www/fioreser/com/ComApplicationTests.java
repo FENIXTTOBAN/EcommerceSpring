476d23e102670989a1746f3e0eacd0653f947213
USUARIO <USUARIO@DESKTOP-13SFIOO>
2024-11-16 17:02:13 -0500

    MI PRIMER ECOMMERCE

---
+++ src/test/java/www/fioreser/com/ComApplicationTests.java
@@ -0,0 +1,13 @@
+package www.fioreser.com;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
